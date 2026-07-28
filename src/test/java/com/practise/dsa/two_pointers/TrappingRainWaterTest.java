98228dd0ac7ea9b5969d67f5876142dfb4fd3be7
bennbenben <bennbenbenl@gmail.com>
2026-07-28 16:28:58 +0800

    feature/two_pointers-trapping_rain_water
    feat(TrappingRainWater): add trapping rain water solution with
    respective junit test

---
+++ src/test/java/com/practise/dsa/two_pointers/TrappingRainWaterTest.java
@@ -0,0 +1,15 @@
+package com.practise.dsa.two_pointers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TrappingRainWaterTest {
+  
+  private final TrappingRainWater trappingRainWater = new TrappingRainWater();
+  
+  @Test
+  void testTrappingRainWater() {
+    int[] height = new int[] {0,2,0,3,1,0,1,3,2,1};
+    Assertions.assertEquals(9, trappingRainWater.trap(height));
+  }
+}
