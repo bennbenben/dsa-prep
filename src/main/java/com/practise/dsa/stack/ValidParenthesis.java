f63974d4c591a2771eb9b9ff5b7247526d4b77e4
bennbenben <bennbenbenl@gmail.com>
2026-08-04 07:45:31 +0800

    feature/stack-valid_parenthesis
    feat(stack/ValidParenthesis): add this solution with junit test

---
+++ src/main/java/com/practise/dsa/stack/ValidParenthesis.java
@@ -0,0 +1,35 @@
+package com.practise.dsa.stack;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class ValidParenthesis {
+
+  public boolean isValid(String s) {
+    System.out.printf("input String s=%s%n",s);
+    Map<Character, Character> complementParenthesis = new HashMap<Character, Character>();
+    complementParenthesis.put('(',')');
+    complementParenthesis.put('{','}');
+    complementParenthesis.put('[',']');
+    System.out.printf("complementParenthesis=%s%n", complementParenthesis.toString());
+    
+    LinkedList<Character> parenthesisStack = new LinkedList<Character>();
+    for (char sChar : s.toCharArray()) {
+      System.out.printf("Existing sChar=%s, parenthesisStack=%s%n", sChar, parenthesisStack.toString());
+      if (complementParenthesis.containsKey(sChar)) {
+        parenthesisStack.offerFirst(sChar);
+      } else {
+        if (parenthesisStack.isEmpty() || !complementParenthesis.get(parenthesisStack.pollFirst()).equals(sChar)) {
+          return false;
+        }
+      }
+    }
+    
+    if (parenthesisStack.isEmpty()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
